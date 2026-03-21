8697124d9974ad02627c701be6c6d9f853dd4707
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2026-03-21 18:54:44 +0100

    Handle api errors

--- src/features/restaurants/api/restaurantClient.spec.ts
+++ src/features/restaurants/api/restaurantClient.spec.ts
@@ -4,34 +4,90 @@ import { restaurantClient } from "./restaurantClient";
 import { http, HttpResponse } from "msw";
 import { setupServer } from "msw/node";
 
-const mockResponse = Array.from({ length: 5 }, () => ({
-  id: simpleFaker.number.int(100),
-  name: faker.company.name(),
-}));
-
-const server = setupServer(
-  http.get("*/restaurants", () => {
-    return HttpResponse.json(mockResponse);
-  }),
-);
-
-beforeAll(() => {
-  // Fail tests if there's an unhandled request to help catch mismatched routes.
-  server.listen({ onUnhandledRequest: "error" });
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
-
 describe("restaurantApiClient", () => {
-  it("should get restaurants from api", async () => {
-    const data = await restaurantClient.get();
+  describe("success case", () => {
+    const mockResponse = Array.from({ length: 5 }, () => ({
+      id: simpleFaker.number.int(100),
+      name: faker.company.name(),
+    }));
 
-    expect(data).toEqual(mockResponse);
+    const server = setupServer(
+      http.get("*/restaurants", () => {
+        return HttpResponse.json(mockResponse);
+      }),
+    );
+
+    beforeAll(() => {
+      // Fail tests if there's an unhandled request to help catch mismatched routes.
+      server.listen({ onUnhandledRequest: "error" });
+    });
+
+    afterEach(() => {
+      server.resetHandlers();
+    });
+
+    afterAll(() => {
+      server.close();
+    });
+
+    it("should get restaurants from api", async () => {
+      const data = await restaurantClient.get();
+
+      expect(data).toEqual(mockResponse);
+    });
+  });
+
+  describe("parsing error", () => {
+    const server = setupServer(
+      http.get("*/restaurants", () => {
+        return HttpResponse.json([{ id: null, name: 99 }]);
+      }),
+    );
+
+    beforeAll(() => {
+      // Fail tests if there's an unhandled request to help catch mismatched routes.
+      server.listen({ onUnhandledRequest: "error" });
+    });
+
+    afterEach(() => {
+      server.resetHandlers();
+    });
+
+    afterAll(() => {
+      server.close();
+    });
+
+    it("should throw a validation error if response does not conform with schema", async () => {
+      await expect(restaurantClient.get()).rejects.toThrowError(
+        "Failed to parse restaurants response",
+      );
+    });
+  });
+
+  describe("generic error", () => {
+    const server = setupServer(
+      http.get("*/restaurants", () => {
+        return new HttpResponse(null, { status: 500 });
+      }),
+    );
+
+    beforeAll(() => {
+      // Fail tests if there's an unhandled request to help catch mismatched routes.
+      server.listen({ onUnhandledRequest: "error" });
+    });
+
+    afterEach(() => {
+      server.resetHandlers();
+    });
+
+    afterAll(() => {
+      server.close();
+    });
+
+    it("should throw a generic error", async () => {
+      await expect(restaurantClient.get()).rejects.toThrowError(
+        /Request failed with status code 500 Internal Server Error/,
+      );
+    });
   });
 });
--- src/features/restaurants/api/restaurantClient.ts
+++ src/features/restaurants/api/restaurantClient.ts
@@ -1,6 +1,7 @@
 import ky from "ky";
 import { restaurantsSchema } from "../model";
 import { ENV } from "varlock/env";
+import { z } from "zod/mini";
 
 const baseURL = `https://api.outsidein.dev/${ENV.API_KEY}`;
 
@@ -9,8 +10,12 @@ export const restaurantClient = Object.freeze({
     try {
       const restaurants = await ky.get(`${baseURL}/restaurants`).json();
       return restaurantsSchema.parse(restaurants);
-    } catch {
-      // TODO: error handling
+    } catch (error: unknown) {
+      if (error instanceof z.core.$ZodError) {
+        console.error(error.issues);
+        throw new Error("Failed to parse restaurants response");
+      }
+      throw error;
     }
   },
 });
