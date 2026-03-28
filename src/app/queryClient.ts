e0fc0fe81613fb02acee6316e101fecc1bdcc2cb
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2026-03-28 11:45:25 +0100

    Add Error Boundary

--- src/app/queryClient.ts
+++ src/app/queryClient.ts
@@ -4,8 +4,15 @@ import { toast } from "sonner";
 export const createQueryClient = (): QueryClient => {
   return new QueryClient({
     queryCache: new QueryCache({
-      onError: (error) => {
-        toast.error(error.message, { position: "top-center", duration: 5_000 });
+      onError: (error, query) => {
+        // only show error toast if we don't have data in the cache
+        // otherwise handle via Error Boundaries
+        if (typeof query.state.data !== "undefined") {
+          toast.error(error.message, {
+            position: "top-center",
+            duration: 5_000,
+          });
+        }
       },
     }),
     defaultOptions: {
@@ -14,6 +21,9 @@ export const createQueryClient = (): QueryClient => {
         gcTime: 1000 * 60 * 30,
         retry: 1,
         refetchOnWindowFocus: false,
+        throwOnError: (_error, query) => {
+          return typeof query.state.data === "undefined";
+        },
       },
       mutations: {
         retry: 0,
